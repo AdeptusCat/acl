1c48670b703b7c203d612446af261e1a2b99697f
unknown <daniel.cordes@zissel.com>
2025-10-26 17:44:34 +0100

    again

--- scenes/game/units/unit_ui.gd
+++ scenes/game/units/unit_ui.gd
@@ -302,7 +302,7 @@ func show_failure():
 func show_success():
 	update_bar(0, 100)
 	_spawn_popup("success")
-	_spawn_flash("success")
+	#_spawn_flash("success")
 	#if detail_ui:
 		#detail_ui.show_success()
 
