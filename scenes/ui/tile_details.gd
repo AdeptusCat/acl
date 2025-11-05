a4a6cef7281a885c783fb02e88e39e73751fe4d1
unknown <daniel.cordes@zissel.com>
2025-11-05 09:30:26 +0100

    before refactor

---
+++ scenes/ui/tile_details.gd
@@ -0,0 +1,201 @@
+extends PanelContainer
+
+
+@export var ground_layer : HexagonTileMapLayer
+
+
+@onready var ground_sprite = $TileDetails/GroundSprite
+@onready var wall_sprite = $TileDetails/WallSprite
+@onready var building_sprite = $TileDetails/BuildingSprite
+@onready var terrain_sprite = $TileDetails/TerrainSprite
+
+@onready var wall_n_sprite = $TileDetails/WallNSprite
+@onready var wall_ne_sprite = $TileDetails/WallNESprite
+@onready var wall_se_sprite = $TileDetails/WallSESprite
+@onready var wall_s_sprite = $TileDetails/WallSSprite
+@onready var wall_sw_sprite = $TileDetails/WallSWSprite
+@onready var wall_nw_sprite = $TileDetails/WallNWSprite
+
+@onready var coverHBoxContainer = $TileCover/CoverHBoxContainer
+
+@onready var tileDetails = $TileDetails
+
+
+@onready var tile_stats = $TileStats
+
+
+var tile_size : Vector2i 
+var detail_zoom_factor : Vector2 = Vector2(2, 2)
+var detail_tile_offset : Vector2 
+
+
+func _ready() -> void:
+	tile_size = ground_layer.tile_set.tile_size
+	detail_tile_offset = (Vector2(tile_size) * detail_zoom_factor) * 0.2
+	tileDetails.size = Vector2(tile_size) * detail_zoom_factor + detail_tile_offset
+	tileDetails.position.y -= tileDetails.size.y + tileDetails.size.x / 10
+	tileDetails.position.x += tileDetails.size.x / 5
+	$TileCover.position.y = tileDetails.position.y + tileDetails.size.y / 2.5
+	$TileCover.position.x = tileDetails.position.x + tileDetails.size.x / 5
+	coverHBoxContainer.scale = detail_zoom_factor * 0.015
+	for child in tile_stats.get_children():
+		child.scale = detail_zoom_factor * 0.015
+	$TileStats/Blocked.scale = detail_zoom_factor * 0.02
+	$TileStats/Hindrance.scale = detail_zoom_factor * 0.02
+	$TileStats/Blocked.position = tileDetails.position + Vector2(tileDetails.size.x / 2, tileDetails.size.y / 4)
+	$TileStats/Hindrance.position = tileDetails.position + Vector2(tileDetails.size.x / 2, tileDetails.size.y / 4)
+	$TileStats/CoverN1.position = tileDetails.position + Vector2(tileDetails.size.x / 2 - tileDetails.size.x / 10, 0)
+	$TileStats/CoverN2.position = tileDetails.position + Vector2(tileDetails.size.x / 2 + tileDetails.size.x / 10 , 0)
+	$TileStats/CoverNW1.position = tileDetails.position + Vector2(0, tileDetails.size.y / 4)
+	$TileStats/CoverNW2.position = tileDetails.position + Vector2(0 + tileDetails.size.x / 7, tileDetails.size.y / 4)
+	$TileStats/CoverSW1.position = tileDetails.position + Vector2(0, tileDetails.size.y / 4 * 3)
+	$TileStats/CoverSW2.position = tileDetails.position + Vector2(0 + tileDetails.size.x / 7, tileDetails.size.y / 4 * 3)
+	$TileStats/CoverS1.position = tileDetails.position + Vector2(tileDetails.size.x / 2 - tileDetails.size.x / 10, tileDetails.size.y)
+	$TileStats/CoverS2.position = tileDetails.position + Vector2(tileDetails.size.x / 2 + tileDetails.size.x / 10 , tileDetails.size.y)
+	$TileStats/CoverSE1.position = tileDetails.position + Vector2(tileDetails.size.x / 6 * 5, tileDetails.size.y / 4 * 3)
+	$TileStats/CoverSE2.position = tileDetails.position + Vector2(tileDetails.size.x / 6 * 5 + tileDetails.size.x / 7, tileDetails.size.y / 4 * 3)
+	$TileStats/CoverNE1.position = tileDetails.position + Vector2(tileDetails.size.x / 6 * 5, tileDetails.size.y / 4)
+	$TileStats/CoverNE2.position = tileDetails.position + Vector2(tileDetails.size.x / 6 * 5 + tileDetails.size.x / 7, tileDetails.size.y / 4)
+
+
+
+func show_tile_data(result: Dictionary):
+	#print(result)
+	result.cover_in_hex
+	result.blocking
+	result.cover_n
+	result.cover_ne
+	result.cover_se
+	result.cover_s
+	result.cover_sw
+	result.cover_nw
+	result.hindrance
+	result.tile_name
+	
+	$TileName.text = result.tile_name
+	for child in tile_stats.get_children():
+		child.visible = false
+	
+	if result.hindrance == true:
+		$TileStats/Hindrance.visible = true
+	
+	if result.blocking == true:
+		$TileStats/Blocked.visible = true
+	
+	if result.cover_n == 1:
+		$Control/TileStats/CoverN1.visible = true
+	elif result.cover_n == 2:
+		$Control/TileStats/CoverN1.visible = true
+		$Control/TileStats/CoverN2.visible = true
+	
+	if result.cover_ne == 1:
+		$Control/TileStats/CoverNE1.visible = true
+	elif result.cover_ne == 2:
+		$Control/TileStats/CoverNE1.visible = true
+		$Control/TileStats/CoverNE2.visible = true
+	
+	if result.cover_se == 1:
+		$Control/TileStats/CoverSE1.visible = true
+	elif result.cover_se == 2:
+		$Control/TileStats/CoverSE1.visible = true
+		$Control/TileStats/CoverSE2.visible = true
+	
+	if result.cover_s == 1:
+		$Control/TileStats/CoverS2.visible = true
+	elif result.cover_s == 2:
+		$Control/TileStats/CoverS1.visible = true
+		$Control/TileStats/CoverS2.visible = true
+	
+	if result.cover_sw == 1:
+		$Control/TileStats/CoverSW2.visible = true
+	elif result.cover_sw == 2:
+		$Control/TileStats/CoverSW1.visible = true
+		$Control/TileStats/CoverSW2.visible = true
+	
+	if result.cover_nw == 1:
+		$Control/TileStats/CoverNW2.visible = true
+	elif result.cover_nw == 2:
+		$Control/TileStats/CoverNW1.visible = true
+		$Control/TileStats/CoverNW2.visible = true
+	
+	for child in coverHBoxContainer.get_children():
+		child.visible = false
+	
+	for cover in range(result.cover_in_hex):
+		coverHBoxContainer.get_children()[cover].visible = true
+		
+	
+	
+	if not result.ground_texture == null:
+		ground_sprite.texture = result.ground_texture
+		ground_sprite.transform = result.ground_texture_transform
+		ground_sprite.scale = ground_sprite.scale * detail_zoom_factor
+	else:
+		ground_sprite.texture = null
+	if not result.wall_texture == null:
+		wall_sprite.texture = result.wall_texture
+		wall_sprite.transform = result.wall_texture_transform
+		wall_sprite.scale = wall_sprite.scale * detail_zoom_factor
+	else:
+		wall_sprite.texture = null
+	if not result.wall_n_texture == null:
+		wall_n_sprite.texture = result.wall_n_texture
+		wall_n_sprite.transform = result.wall_n_texture_transform
+		wall_n_sprite.scale = wall_n_sprite.scale * detail_zoom_factor
+		wall_n_sprite.position.y -= tile_size.y * detail_zoom_factor.y
+	else:
+		wall_n_sprite.texture = null
+	if not result.wall_ne_texture == null:
+		wall_ne_sprite.texture = result.wall_ne_texture
+		wall_ne_sprite.transform = result.wall_ne_texture_transform
+		wall_ne_sprite.scale = wall_ne_sprite.scale * detail_zoom_factor
+		wall_ne_sprite.position.x += tile_size.x * detail_zoom_factor.x / 1.5
+		wall_ne_sprite.position.y -= tile_size.y / 2 * detail_zoom_factor.y
+	else:
+		wall_ne_sprite.texture = null
+	if not result.wall_se_texture == null:
+		wall_se_sprite.texture = result.wall_se_texture
+		wall_se_sprite.transform = result.wall_se_texture_transform
+		wall_se_sprite.scale = wall_se_sprite.scale * detail_zoom_factor
+		wall_se_sprite.position.x += tile_size.x  * detail_zoom_factor.x / 1.5
+		wall_se_sprite.position.y += tile_size.y / 2 * detail_zoom_factor.y
+	else:
+		wall_se_sprite.texture = null
+	if not result.wall_s_texture == null:
+		wall_s_sprite.texture = result.wall_s_texture
+		wall_s_sprite.transform = result.wall_s_texture_transform
+		wall_s_sprite.scale = wall_s_sprite.scale * detail_zoom_factor
+		wall_s_sprite.position.y += tile_size.y * detail_zoom_factor.y
+	else:
+		wall_s_sprite.texture = null
+	if not result.wall_sw_texture == null:
+		wall_sw_sprite.texture = result.wall_sw_texture
+		wall_sw_sprite.transform = result.wall_sw_texture_transform
+		wall_sw_sprite.scale = wall_sw_sprite.scale * detail_zoom_factor
+		wall_sw_sprite.position.x -= tile_size.x  * detail_zoom_factor.x / 1.5
+		wall_sw_sprite.position.y += tile_size.y / 2 * detail_zoom_factor.y
+	else:
+		wall_sw_sprite.texture = null
+	if not result.wall_nw_texture == null:
+		wall_nw_sprite.texture = result.wall_nw_texture
+		wall_nw_sprite.transform = result.wall_nw_texture_transform
+		wall_nw_sprite.scale = wall_nw_sprite.scale * detail_zoom_factor
+		wall_nw_sprite.position.x -= tile_size.x  * detail_zoom_factor.x / 1.5
+		wall_nw_sprite.position.y -= tile_size.y / 2 * detail_zoom_factor.y
+	else:
+		wall_nw_sprite.texture = null
+	if not result.building_texture == null:
+		building_sprite.texture = result.building_texture
+		building_sprite.transform = result.building_texture_transform
+		building_sprite.scale = building_sprite.scale * detail_zoom_factor
+	else:
+		building_sprite.texture = null
+	if not result.terrain_texture == null:
+		terrain_sprite.texture = result.terrain_texture
+		terrain_sprite.transform = result.terrain_texture_transform
+		terrain_sprite.scale = terrain_sprite.scale * detail_zoom_factor
+	else:
+		terrain_sprite.texture = null
+	
+	for child in tileDetails.get_children():
+		child.position += (Vector2(tile_size) * detail_zoom_factor) / 2 + (detail_tile_offset / 2)
--- scenes/ui/ui.gd
+++ scenes/ui/ui.gd
@@ -1,28 +1,11 @@
 extends CanvasLayer
 
-@export var ground_layer : HexagonTileMapLayer
 @export var unit_stats_details_scene : PackedScene
 
 @onready var countdown_panel_container = $Control/Countdown
-@onready var ground_sprite = $Control/TileDetails/GroundSprite
-@onready var wall_sprite = $Control/TileDetails/WallSprite
-@onready var building_sprite = $Control/TileDetails/BuildingSprite
-@onready var terrain_sprite = $Control/TileDetails/TerrainSprite
 
-@onready var wall_n_sprite = $Control/TileDetails/WallNSprite
-@onready var wall_ne_sprite = $Control/TileDetails/WallNESprite
-@onready var wall_se_sprite = $Control/TileDetails/WallSESprite
-@onready var wall_s_sprite = $Control/TileDetails/WallSSprite
-@onready var wall_sw_sprite = $Control/TileDetails/WallSWSprite
-@onready var wall_nw_sprite = $Control/TileDetails/WallNWSprite
-
-@onready var coverHBoxContainer = $Control/TileCover/CoverHBoxContainer
-
-@onready var tileDetails = $Control/TileDetails
 
 @onready var cover_icon_scene = preload("res://scenes/ui/cover_icon.tscn")
-
-@onready var tile_stats = $Control/TileStats
 
 @onready var target_cover_distance = $Control/TargetCoverDistance
 @onready var cover_container = $Control/TargetCoverDistance/VBoxContainer/Cover
@@ -40,37 +23,9 @@ const HEX_DIRECTIONS = [
 ]
 
 
-var tile_size : Vector2i 
-var detail_zoom_factor : Vector2 = Vector2(2, 2)
-var detail_tile_offset : Vector2 
+
 
 func _ready() -> void:
-	tile_size = ground_layer.tile_set.tile_size
-	detail_tile_offset = (Vector2(tile_size) * detail_zoom_factor) * 0.2
-	tileDetails.size = Vector2(tile_size) * detail_zoom_factor + detail_tile_offset
-	tileDetails.position.y -= tileDetails.size.y + tileDetails.size.x / 10
-	tileDetails.position.x += tileDetails.size.x / 5
-	$Control/TileCover.position.y = tileDetails.position.y + tileDetails.size.y / 2.5
-	$Control/TileCover.position.x = tileDetails.position.x + tileDetails.size.x / 5
-	coverHBoxContainer.scale = detail_zoom_factor * 0.015
-	for child in tile_stats.get_children():
-		child.scale = detail_zoom_factor * 0.015
-	$Control/TileStats/Blocked.scale = detail_zoom_factor * 0.02
-	$Control/TileStats/Hindrance.scale = detail_zoom_factor * 0.02
-	$Control/TileStats/Blocked.position = tileDetails.position + Vector2(tileDetails.size.x / 2, tileDetails.size.y / 4)
-	$Control/TileStats/Hindrance.position = tileDetails.position + Vector2(tileDetails.size.x / 2, tileDetails.size.y / 4)
-	$Control/TileStats/CoverN1.position = tileDetails.position + Vector2(tileDetails.size.x / 2 - tileDetails.size.x / 10, 0)
-	$Control/TileStats/CoverN2.position = tileDetails.position + Vector2(tileDetails.size.x / 2 + tileDetails.size.x / 10 , 0)
-	$Control/TileStats/CoverNW1.position = tileDetails.position + Vector2(0, tileDetails.size.y / 4)
-	$Control/TileStats/CoverNW2.position = tileDetails.position + Vector2(0 + tileDetails.size.x / 7, tileDetails.size.y / 4)
-	$Control/TileStats/CoverSW1.position = tileDetails.position + Vector2(0, tileDetails.size.y / 4 * 3)
-	$Control/TileStats/CoverSW2.position = tileDetails.position + Vector2(0 + tileDetails.size.x / 7, tileDetails.size.y / 4 * 3)
-	$Control/TileStats/CoverS1.position = tileDetails.position + Vector2(tileDetails.size.x / 2 - tileDetails.size.x / 10, tileDetails.size.y)
-	$Control/TileStats/CoverS2.position = tileDetails.position + Vector2(tileDetails.size.x / 2 + tileDetails.size.x / 10 , tileDetails.size.y)
-	$Control/TileStats/CoverSE1.position = tileDetails.position + Vector2(tileDetails.size.x / 6 * 5, tileDetails.size.y / 4 * 3)
-	$Control/TileStats/CoverSE2.position = tileDetails.position + Vector2(tileDetails.size.x / 6 * 5 + tileDetails.size.x / 7, tileDetails.size.y / 4 * 3)
-	$Control/TileStats/CoverNE1.position = tileDetails.position + Vector2(tileDetails.size.x / 6 * 5, tileDetails.size.y / 4)
-	$Control/TileStats/CoverNE2.position = tileDetails.position + Vector2(tileDetails.size.x / 6 * 5 + tileDetails.size.x / 7, tileDetails.size.y / 4)
 	#var db = AudioServer.get_bus_volume_db(AudioServer.get_bus_index("Master"))
 	##var db = -2
 	#var ratio = pow(10.0, db / 20.0)
@@ -144,147 +99,6 @@ func _on_show_unit_details(unit: Unit):
 
 func _on_hide_unit_details():
 	unit_details.hide_unit_detail()
-
-func show_tile_data(result: Dictionary):
-	#print(result)
-	result.cover_in_hex
-	result.blocking
-	result.cover_n
-	result.cover_ne
-	result.cover_se
-	result.cover_s
-	result.cover_sw
-	result.cover_nw
-	result.hindrance
-	result.tile_name
-	
-	$Control/TileName.text = result.tile_name
-	for child in tile_stats.get_children():
-		child.visible = false
-	
-	if result.hindrance == true:
-		$Control/TileStats/Hindrance.visible = true
-	
-	if result.blocking == true:
-		$Control/TileStats/Blocked.visible = true
-	
-	if result.cover_n == 1:
-		$Control/TileStats/CoverN1.visible = true
-	elif result.cover_n == 2:
-		$Control/TileStats/CoverN1.visible = true
-		$Control/TileStats/CoverN2.visible = true
-	
-	if result.cover_ne == 1:
-		$Control/TileStats/CoverNE1.visible = true
-	elif result.cover_ne == 2:
-		$Control/TileStats/CoverNE1.visible = true
-		$Control/TileStats/CoverNE2.visible = true
-	
-	if result.cover_se == 1:
-		$Control/TileStats/CoverSE1.visible = true
-	elif result.cover_se == 2:
-		$Control/TileStats/CoverSE1.visible = true
-		$Control/TileStats/CoverSE2.visible = true
-	
-	if result.cover_s == 1:
-		$Control/TileStats/CoverS2.visible = true
-	elif result.cover_s == 2:
-		$Control/TileStats/CoverS1.visible = true
-		$Control/TileStats/CoverS2.visible = true
-	
-	if result.cover_sw == 1:
-		$Control/TileStats/CoverSW2.visible = true
-	elif result.cover_sw == 2:
-		$Control/TileStats/CoverSW1.visible = true
-		$Control/TileStats/CoverSW2.visible = true
-	
-	if result.cover_nw == 1:
-		$Control/TileStats/CoverNW2.visible = true
-	elif result.cover_nw == 2:
-		$Control/TileStats/CoverNW1.visible = true
-		$Control/TileStats/CoverNW2.visible = true
-	
-	for child in coverHBoxContainer.get_children():
-		child.visible = false
-	
-	for cover in range(result.cover_in_hex):
-		coverHBoxContainer.get_children()[cover].visible = true
-		
-	
-	
-	if not result.ground_texture == null:
-		ground_sprite.texture = result.ground_texture
-		ground_sprite.transform = result.ground_texture_transform
-		ground_sprite.scale = ground_sprite.scale * detail_zoom_factor
-	else:
-		ground_sprite.texture = null
-	if not result.wall_texture == null:
-		wall_sprite.texture = result.wall_texture
-		wall_sprite.transform = result.wall_texture_transform
-		wall_sprite.scale = wall_sprite.scale * detail_zoom_factor
-	else:
-		wall_sprite.texture = null
-	if not result.wall_n_texture == null:
-		wall_n_sprite.texture = result.wall_n_texture
-		wall_n_sprite.transform = result.wall_n_texture_transform
-		wall_n_sprite.scale = wall_n_sprite.scale * detail_zoom_factor
-		wall_n_sprite.position.y -= tile_size.y * detail_zoom_factor.y
-	else:
-		wall_n_sprite.texture = null
-	if not result.wall_ne_texture == null:
-		wall_ne_sprite.texture = result.wall_ne_texture
-		wall_ne_sprite.transform = result.wall_ne_texture_transform
-		wall_ne_sprite.scale = wall_ne_sprite.scale * detail_zoom_factor
-		wall_ne_sprite.position.x += tile_size.x * detail_zoom_factor.x / 1.5
-		wall_ne_sprite.position.y -= tile_size.y / 2 * detail_zoom_factor.y
-	else:
-		wall_ne_sprite.texture = null
-	if not result.wall_se_texture == null:
-		wall_se_sprite.texture = result.wall_se_texture
-		wall_se_sprite.transform = result.wall_se_texture_transform
-		wall_se_sprite.scale = wall_se_sprite.scale * detail_zoom_factor
-		wall_se_sprite.position.x += tile_size.x  * detail_zoom_factor.x / 1.5
-		wall_se_sprite.position.y += tile_size.y / 2 * detail_zoom_factor.y
-	else:
-		wall_se_sprite.texture = null
-	if not result.wall_s_texture == null:
-		wall_s_sprite.texture = result.wall_s_texture
-		wall_s_sprite.transform = result.wall_s_texture_transform
-		wall_s_sprite.scale = wall_s_sprite.scale * detail_zoom_factor
-		wall_s_sprite.position.y += tile_size.y * detail_zoom_factor.y
-	else:
-		wall_s_sprite.texture = null
-	if not result.wall_sw_texture == null:
-		wall_sw_sprite.texture = result.wall_sw_texture
-		wall_sw_sprite.transform = result.wall_sw_texture_transform
-		wall_sw_sprite.scale = wall_sw_sprite.scale * detail_zoom_factor
-		wall_sw_sprite.position.x -= tile_size.x  * detail_zoom_factor.x / 1.5
-		wall_sw_sprite.position.y += tile_size.y / 2 * detail_zoom_factor.y
-	else:
-		wall_sw_sprite.texture = null
-	if not result.wall_nw_texture == null:
-		wall_nw_sprite.texture = result.wall_nw_texture
-		wall_nw_sprite.transform = result.wall_nw_texture_transform
-		wall_nw_sprite.scale = wall_nw_sprite.scale * detail_zoom_factor
-		wall_nw_sprite.position.x -= tile_size.x  * detail_zoom_factor.x / 1.5
-		wall_nw_sprite.position.y -= tile_size.y / 2 * detail_zoom_factor.y
-	else:
-		wall_nw_sprite.texture = null
-	if not result.building_texture == null:
-		building_sprite.texture = result.building_texture
-		building_sprite.transform = result.building_texture_transform
-		building_sprite.scale = building_sprite.scale * detail_zoom_factor
-	else:
-		building_sprite.texture = null
-	if not result.terrain_texture == null:
-		terrain_sprite.texture = result.terrain_texture
-		terrain_sprite.transform = result.terrain_texture_transform
-		terrain_sprite.scale = terrain_sprite.scale * detail_zoom_factor
-	else:
-		terrain_sprite.texture = null
-	
-	for child in tileDetails.get_children():
-		child.position += (Vector2(tile_size) * detail_zoom_factor) / 2 + (detail_tile_offset / 2)
 
 
 func _on_try_again_button_pressed() -> void:
